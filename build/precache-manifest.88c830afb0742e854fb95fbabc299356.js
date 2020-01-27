57ba0c053d8b72e435d8a163cfa83a9415d30099
suchana <suchanachakraborty@gmail.com>
2020-01-27 21:29:11 +0530

    The frontend

---
+++ build/precache-manifest.88c830afb0742e854fb95fbabc299356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c20acf68c7fbe2455933de4d8dc6b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a64eb8aaad3bff84c019",
+    "url": "/static/css/2.9dafe002.chunk.css"
+  },
+  {
+    "revision": "8a0e704470d99c60cf52",
+    "url": "/static/css/main.eb882395.chunk.css"
+  },
+  {
+    "revision": "a64eb8aaad3bff84c019",
+    "url": "/static/js/2.99c889ef.chunk.js"
+  },
+  {
+    "revision": "8a0e704470d99c60cf52",
+    "url": "/static/js/main.7bef1143.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
